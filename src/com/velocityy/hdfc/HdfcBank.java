46912112d1e4a449a35e09a9caf09ec467889444
shailesh <shinde78755566@gmail.com>
2023-07-11 22:27:57 +0530

    code changes

---
+++ src/com/velocityy/hdfc/HdfcBank.java
@@ -0,0 +1,12 @@
+package com.velocityy.hdfc;
+
+public class HdfcBank {
+
+	public static void main(String[] args) {
+		
+	
+		System.out.println("My fist phase working dict");
+
+	}
+
+}
---
+++ src/com/velocityy/hdfc/Hello.java
@@ -0,0 +1,12 @@
+package com.velocityy.hdfc;
+
+public class Hello {
+
+	public static void main(String[] args) {
+
+
+		
+		System.out.println("hello shailesh");
+	}
+
+}
